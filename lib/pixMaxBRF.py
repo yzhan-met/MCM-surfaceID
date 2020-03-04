c0258fe0de5ac6926acc08b46075d5842ee050bb
yzhan-met <yizhe.git@gmail.com>
2020-03-04 15:12:48 +1300

    init

--- lib/pixMaxBRF.py
+++ lib/pixMaxBRF.py
@@ -102,93 +102,87 @@ def pix_max_BRF(DoY):
 
     # open MAIAC dataset for the PTA at the DOY
     MAIAC_folder = config.get('general', 'MAIAC_folder')
-    PTA = config.get('general', 'PTA_name')
+    PTA = config.get('general', 'PTA')
     path_MAIAC = os.path.join(MAIAC_folder, "MAIA_BRDF_{0}_*{1}.hdf".format(PTA, str(DoY).zfill(3)))
-    DSS = xr.open_mfdataset(path_MAIAC, concat_dim='yr')
+    DSS = xr.open_mfdataset(path_MAIAC, concat_dim='yr', combine='nested')
 
-    print(DSS.Kiso)
+    # choose MODIS band 1 (red)
     k_iso = DSS.Kiso[:, 0, :, :].values  # (15, 1000, 1000)
     k_vol = DSS.Kvol[:, 0, :, :].values
     k_geo = DSS.Kgeo[:, 0, :, :].values
 
-    # # ============quality control============
-    # # all invalid values are set to np.nan so that no target_max_brf will be calculated
-    #
-    # # Check RTLS coefficients
-    # idx = np.where((k_iso < -1) | (k_vol < -1) | (k_geo < -1))
-    # k_iso[idx] = np.nan
-    # k_vol[idx] = np.nan
-    # k_geo[idx] = np.nan
-    #
-    # # Define MAIA water and coastal surface types
-    # # used in the main loop
-    # cat_water = [0, 3, 5, 6]
-    # cat_coastal = [2, 4]
-    #
-    # # ===========main process===========
-    # #                          x   y   SZA VZA RAZ
-    # # the result array is  (1000, 1000, 10, 15, 12)
-    # # this shape needs to be changed with changes in {kernels_MAIA} function
-    # num_y, num_x = lat.shape
-    # num_SZA = len(SZA)
-    # num_VZA = len(VZA)
-    # num_RAZ = len(RAZ)
-    #
-    # target_max_brf = np.zeros((num_y, num_x, num_SZA, num_VZA, num_RAZ))
-    # target_filled = np.ones((num_SZA, num_VZA, num_RAZ)) * -999.
-    # target_water = np.ones((num_SZA, num_VZA, num_RAZ)) * -998.
-    # target_coastal = np.ones((num_SZA, num_VZA, num_RAZ)) * -997.
-    #
-    # # line loop
-    # for i in tqdm(range(num_y)):
-    #     # grid loop
-    #     for j in range(num_x):
-    #
-    #         # check MAIA water category mask
-    #         if lw_mask[i, j] in cat_water:
-    #             target_max_brf[i, j] = target_water
-    #         # check MAIA coastal category mask
-    #         elif lw_mask[i, j] in cat_coastal:
-    #             target_max_brf[i, j] = target_coastal
-    #         # calculate max BRF for rest pixels
-    #         else:
-    #             coeff_iso = k_iso[:, i, j]
-    #             coeff_vol = k_vol[:, i, j]
-    #             coeff_geo = k_geo[:, i, j]
-    #
-    #             # define number of invalid years by accounting for unavailable coeff_iso
-    #             # (same as coeff_vol and coeff_geo)
-    #             invalid_yrs = np.sum(xr.ufuncs.isnan(k_iso[:, i, j]))
-    #             # do not retrieve BRFs if the number of invalid years is greater than 5
-    #             if invalid_yrs > 5:
-    #                 # print("sorry to see but there ({}, {}) at DoY-{} is bad...".format(i, j, DoY))
-    #                 target_max_brf[i, j] = target_filled
-    #             else:
-    #                 # SZA loop
-    #                 # for each SZA @ each pixel, retrieve the max BRF for all VZA and RAZ bins
-    #                 for k in range(num_SZA):
-    #                     kern_ross = ROSS_kernels[k]
-    #                     kern_li = LI_kernels[k]
-    #                     target_max_brf[i, j, k] = max_brf_retrieve_fast(coeff_iso, coeff_vol, coeff_geo, kern_ross,
-    #                                                                     kern_li)
-    #
-    # # ===========save results===========
-    # # convert result array to xarray
-    # target_max_brf = xr.DataArray(target_max_brf, name='max_BRF',
-    #                               coords=[('y', np.arange(num_y)), ('x', np.arange(num_x)), ('cos_sza', cos_sza),
-    #                                       ('vza', VZA), ('raz', RAZ)])
-    # target_max_brf.encoding['_FillValue'] = -999.
-    #
-    # # save xarray to nc
-    # out_nc = 'MAIAC_SMB/maxBRF_{}_{}.nc'.format(PTA, str(DoY).zfill(3))
-    # target_max_brf.to_netcdf(out_nc, 'w')
-    # lat.to_netcdf(out_nc, 'a')
-    # lon.to_netcdf(out_nc, 'a')
-    #
-    # return out_nc
+    # do some quality controls (invalid values are set to np.nan so that no target_max_brf will be calculated)
+    # check RTLS coefficients
+    idx = np.where((k_iso < -1) | (k_vol < -1) | (k_geo < -1))
+    k_iso[idx] = np.nan
+    k_vol[idx] = np.nan
+    k_geo[idx] = np.nan
 
+    # define MAIA water and coastal surface types used in the main loop
+    cat_water = [0, 3, 5, 6]
+    cat_coastal = [2, 4]
 
-if __name__ == '__main__':
+    # main loop
+    #                          x   y   SZA VZA RAZ
+    # the result array is  (1000, 1000, 10, 15, 12)
+    # this shape needs to be changed with changes in {kernels_MAIA} function
+    num_y, num_x = lat.shape
+    num_SZA = len(sza)
+    num_VZA = len(vza)
+    num_RAZ = len(raz)
 
-    for doy in np.arange(16, 366, 8):
-        pix_max_BRF('LA', doy)
+    target_max_brf = np.zeros((num_y, num_x, num_SZA, num_VZA, num_RAZ))
+    target_filled = np.ones((num_SZA, num_VZA, num_RAZ)) * -999.
+    target_water = np.ones((num_SZA, num_VZA, num_RAZ)) * -998.
+    target_coastal = np.ones((num_SZA, num_VZA, num_RAZ)) * -997.
+
+    # line loop
+    for i in tqdm(range(num_y)):
+        # grid loop
+        for j in range(num_x):
+
+            # check MAIA water category mask
+            if lw_mask[i, j] in cat_water:
+                target_max_brf[i, j] = target_water
+            # check MAIA coastal category mask
+            elif lw_mask[i, j] in cat_coastal:
+                target_max_brf[i, j] = target_coastal
+            # calculate max BRF for rest pixels
+            else:
+                coeff_iso = k_iso[:, i, j]
+                coeff_vol = k_vol[:, i, j]
+                coeff_geo = k_geo[:, i, j]
+
+                # define number of invalid years by accounting for unavailable coeff_iso
+                # (same as coeff_vol and coeff_geo)
+                invalid_yrs = np.sum(xr.ufuncs.isnan(k_iso[:, i, j]))
+                # do not retrieve BRFs if the number of invalid years is greater than max_invalid_yrs
+                max_invalid_yrs = config.get('mainLoop', 'max_invalid_yrs')
+                if invalid_yrs > max_invalid_yrs:
+                    # print("sorry to see but there ({}, {}) at DoY-{} is bad...".format(i, j, DoY))
+                    target_max_brf[i, j] = target_filled
+                else:
+                    # SZA loop
+                    # for each SZA @ each pixel, retrieve the max BRF for all VZA and RAZ bins
+                    for k in range(num_SZA):
+                        kern_ross = ROSS_kernels[k]
+                        kern_li = LI_kernels[k]
+                        target_max_brf[i, j, k] = \
+                            max_brf_retrieve_fast(coeff_iso, coeff_vol, coeff_geo, kern_ross, kern_li)
+
+    # save results
+    # convert result array to xarray
+    target_max_brf = xr.DataArray(target_max_brf, name='max_BRF',
+                                  coords=[('y', np.arange(num_y)),
+                                          ('x', np.arange(num_x)),
+                                          ('cos_sza', cos_sza), ('vza', vza), ('raz', raz)])
+    target_max_brf.encoding['_FillValue'] = -999.
+
+    # save xarray to nc
+    SMB_folder = config.get('general', 'SMB_folder')
+    smb_file = os.path.join(SMB_folder, 'maxBRF_{}_{}.nc'.format(PTA, str(DoY).zfill(3)))
+    target_max_brf.to_netcdf(smb_file, 'w')
+    lat.to_netcdf(smb_file, 'a')
+    lon.to_netcdf(smb_file, 'a')
+
+    return smb_file
--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 from lib.pixMaxBRF import pix_max_BRF
 
 
-pix_max_BRF('LA', 111)
+pix_max_BRF(8)
