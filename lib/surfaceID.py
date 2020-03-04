4d4b4ba17fd8869a208ba6ad780f30ab3a1cf542
yzhan-met <yizhe.git@gmail.com>
2020-03-04 16:50:47 +1300

    init

--- lib/surfaceID.py
+++ lib/surfaceID.py
@@ -47,8 +47,7 @@ def surface_id(file_smb):
 
     # select data
     print("[surfaceID.py] select smb subsets for clustering")
-    sel_data = DS.max_BRF.sel(sza=sel_cos_sza,
-                              vza=sel_vza,
+    sel_data = DS.max_BRF.sel(vza=sel_vza,
                               raz=sel_raz)
     filled_sel_data = sel_data.fillna(-996.)
 
